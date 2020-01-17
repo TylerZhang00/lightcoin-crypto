be6e7d200f5416eb44d6823c88a8639748ed4482
Tyler <yiqizhang@gmail.com>
2020-01-17 22:12:41 +0000

    done starting/ending no stretch

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-let balance = 500.00;
+let balance = 500.0;
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
   }
-
-  commit() {
-    balance -= this.amount;
-  }
-
 }
 
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+    console.log("test", this.account);
+  }
+  commit() {
+    console.log("test2", this.account.balance);
+    this.account.balance += this.value;
+  }
+}
 
+class Deposit extends Transaction {
+  get value() {
+    // console.log("test3", this.amount);
+    return this.amount;
+  }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    // console.log("test3", this.amount);
+    return -this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+console.log("Starting Balance:", myAccount.balance);
+
+const t1 = new Deposit(120.0, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.0, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log("Ending Balance:", myAccount.balance);
